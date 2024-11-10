975086f8f1c0aa61b76382b9ed418618c09fdfa6
Mang Tri <mangtri93@gmail.com>
2024-11-10 21:46:01 +0800

    update lagi

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,33 +1,33 @@
 export default defineEventHandler(async (event) => {
-  if (!event.path.startsWith("/api")) return;
-  if (event.path.startsWith("/api/auth/login")) return;
-  if (event.path.startsWith("/api/auth/signup")) return;
+  // if (!event.path.startsWith("/api")) return;
+  // if (event.path.startsWith("/api/auth/login")) return;
+  // if (event.path.startsWith("/api/auth/signup")) return;
 
-  const session = await getUserSession(event)
+  // const session = await getUserSession(event)
 
-  console.log('session:', session.secure)
+  // console.log('session:', session.secure)
 
-  const authHeader = event.node.req.headers.authorization
+  // const authHeader = event.node.req.headers.authorization
 
-  if(!authHeader) {
-    return createError({
-      statusCode: 401,
-      statusMessage: 'Token is required'
-    })
-  }
+  // if(!authHeader) {
+  //   return createError({
+  //     statusCode: 401,
+  //     statusMessage: 'Token is required'
+  //   })
+  // }
 
-  const getToken = authHeader.split(' ')[1]
+  // const getToken = authHeader.split(' ')[1]
 
-  const verifyJwtToken = verifyToken(getToken)
+  // const verifyJwtToken = verifyToken(getToken)
 
-  // if token is invalid
-  if (!verifyJwtToken) {
-    event.context.user = null
-    return createError({
-      statusCode: 401,
-      statusMessage: 'Invalid token'
-    })
-  }
+  // // if token is invalid
+  // if (!verifyJwtToken) {
+  //   event.context.user = null
+  //   return createError({
+  //     statusCode: 401,
+  //     statusMessage: 'Invalid token'
+  //   })
+  // }
 
-  event.context.user = decodeToken(getToken)
+  // event.context.user = decodeToken(getToken)
 })
